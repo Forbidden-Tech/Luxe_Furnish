05889f2a219672ccf6536a99a7ecf550256200b9
MbakzArr <mbadaligab@gmail.com>
2025-11-30 21:04:56 +0200

    Changed Contact Us color button

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -211,7 +211,7 @@ export default function About() {
                 </Button>
               </Link>
               <Link to={createPageUrl('Contact')}>
-                <Button size="lg" variant="outline" className="border-white/30 text-white hover:bg-white/10 rounded-full px-8">
+                <Button size="lg" variant="outline" className="border-white/30 text-black hover:bg-white/10 rounded-full px-8">
                   Contact Us
                 </Button>
               </Link>
